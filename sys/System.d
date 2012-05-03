8fe27ea1d221be7b3be2df00e8c4fb5e594e7b26
Jacob Carlborg <doob@me.com>
2012-05-03 19:28:23 +0200

    Add a System module.

--- sys/System.d
+++ sys/System.d
@@ -1,13 +1,15 @@
 /**
  * Copyright: Copyright (c) 2012 Jacob Carlborg. All rights reserved.
  * Authors: Jacob Carlborg
- * Version: Initial created: Feb 24, 2012
+ * Version: Initial created: May 3, 2012
  * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
- * 
  */
-module mambo.sys._;
+module mambo.sys.System;
 
-public:
+import core.stdc.stdlib : EXIT_SUCCESS, EXIT_FAILURE;
 
-import mambo.sys.Process;
-import mambo.sys.System;
+enum ExitCode
+{
+	success = EXIT_SUCCESS,
+	failure = EXIT_FAILURE
+}

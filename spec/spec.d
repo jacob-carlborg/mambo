af4dd02e15c01a1429075017c9a4ed9255e4f06b
Jacob Carlborg <doob@me.com>
2012-12-20 08:54:42 +0100

    Remove workaround for unit test conflict.

--- spec/spec.d
+++ spec/spec.d
@@ -6,35 +6,9 @@
  */
 module spec.spec;
 
-import tango.core.Runtime;
-
 import dspec.ExampleGroup;
-
-shared static this ()
-{
-	Runtime.moduleUnitTester = &unitTestRunner;
-}
-
-/*
- * The tests that test for XML with attributes are not completely
- * reliable, due to the XML module in Phobos saves the XML
- * attributes in an associative array.
- */
-bool unitTestRunner ()
-{
-	foreach (m ; ModuleInfo)
-	{
-		if (m)
-		{
-			if (auto fp = m.unitTest)
-				fp();
-		}
-	}
-
-	return ExampleGroup.instance.run();
-}
 
 void main ()
 {
-	// Dummy
+	ExampleGroup.instance.run();
 }
